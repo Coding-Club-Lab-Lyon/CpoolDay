285904f4d3d4f547d0b2400017fa983080172ecc
Le-roi-Hatoku <167294323+Le-roi-Hatoku@users.noreply.github.com>
2024-04-17 12:53:16 +0000

    [fix] exo1

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,18 +1,10 @@
-coucou je sais toujours pas quoi dire x)
 void print_char(char c);
 
 void my_print_ascii(void)
     {
-for(int compteur = 32 ; compteur < 127 ; compteur++)
+for(int compteur = 33 ; compteur <= 127 ; compteur++)
     {
-        print_char(compteur)
+        print_char(compteur);
     }
 
     }
-
-int main()
-{
-my_print_ascii();
-return 0;
-
-}
